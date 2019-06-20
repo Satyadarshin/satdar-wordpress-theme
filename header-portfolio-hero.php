4cbb8a912b9f5d927b91a6b2e9d8ae2e6330ab59
Satyadarshin Perry <stdn.engineer@gmail.com>
2019-06-20 17:08:11 +0100

    Big inroad into banner layout, but it doesn't respond gracefully.

--- header-portfolio-hero.php
+++ header-portfolio-hero.php
@@ -1,13 +1,16 @@
 <section class="hero odysseus">
-    
-    <div class="row intro">
-            <img src="http://localhost:8888/Satyadarshin/stdn_wordpress/wp-content/themes/satdar-wordpress-theme/images/www.svg" alt="">
-        <div class="small-centered medium-uncentered medium-6 large-6 columns site_name">
-            <h1><a href="/"><?php bloginfo('name'); ?></a></h1>
-            <p><?php bloginfo('description'); ?></p>
-        </div>
-        <div class="small-centered medium-uncentered medium-6 large-6 columns">
-            <div class="rocketman"></div>
+    <div class="row">
+        <div class="large-12 columns">
+            <div class="intro">
+                <img src="http://localhost:8888/Satyadarshin/stdn_wordpress/wp-content/themes/satdar-wordpress-theme/images/www.svg" alt="">
+                <div class="branding">
+                    <div class="site_name">
+                        <h1><a href="/"><?php bloginfo('name'); ?></a></h1>
+                        <p><?php bloginfo('description'); ?></p>
+                    </div>
+                    <div class="rocketman"></div>
+                </div>
+            </div>
         </div>
     </div>
 </section>
