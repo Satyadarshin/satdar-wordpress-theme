601d55a596c880c422c502579c246d6ed3930790
Satyadarshin Perry <stdn.engineer@gmail.com>
2019-08-01 14:09:55 +0100

    Steps towards respsonive, three-panel homepage service highlight boxes. Finding difficulty with <large sizes where the layout of boxes seems to fail.

--- header-hero.php
+++ header-hero.php
@@ -1,11 +1,11 @@
 <section class="hero <?php echo (is_front_page()) ? "jason" : "theseus"; ?>">
     <div class="row intro">
-        <div class="small-centered medium-uncentered medium-6 large-6 columns">
-            <h1><a <?php echo (is_front_page()) ? "class=\"perseus\"" : ""; ?> href="<?php echo (is_front_page()) ? "#nav-top" : "/"; ?>"><?php bloginfo('name'); ?></a></h1>
-            <p><?php bloginfo('description'); ?></p>
-        </div>
-        <div class="small-centered medium-uncentered medium-6 large-6 columns">
-            <div class="rocketman"><a <?php echo (is_front_page()) ? "class=\"perseus\"" : ""; ?> href="<?php echo (is_front_page()) ? "#nav-top" : "/"; ?>"></a></div>
+        <div class="large-12 columns">
+            <h1>
+                <a <?php echo (is_front_page()) ? "class=\"perseus\"" : ""; ?> href="<?php echo (is_front_page()) ? "#nav-top" : "/"; ?>">        
+                    <img src="http://localhost:8888/Satyadarshin/stdn_wordpress/wp-content/themes/satdar-wordpress-theme/images/SatyadarshinDotCom.png" alt="Satyadarshin Perry, Web Content Developer, Graphic Designer, and Artist">
+                </a>
+            </h1>
         </div>
     </div>
 </section>
