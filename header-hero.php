3bf47f534f06ea9b817d205e180e7b478d10f85d
Satyadarshin Perry <stdn.engineer@gmail.com>
2019-10-19 17:25:19 +0100

    added horizontal www.sataydarshin.com lettering to homepage landscape orinetation

--- header-hero.php
+++ header-hero.php
@@ -5,7 +5,7 @@
             <h1>
                 <a <?php echo (is_front_page()) ? "class=\"perseus\"" : ""; ?> href="<?php echo (is_front_page()) ? "#nav-top" : "/"; ?>"> 
                     <img class="deep_viewport" src="https://www.satyadarshin.com/wp-content/themes/satdar-wordpress-theme/images/SatyaDarshinDotCom.png" alt="Satyadarshin Perry, Web Content Developer, Graphic Designer, and Artist">
-                    <img class="narrow_viewport" src="https://www.satyadarshin.com/wp-content/themes/satdar-wordpress-theme/images/www.svg" alt="">
+                    <img class="narrow_viewport" src="https://www.satyadarshin.com/wp-content/themes/satdar-wordpress-theme/images/SatyaDarshinDotCom_wide.png" alt="">
                 </a>
             </h1>
         </div>
