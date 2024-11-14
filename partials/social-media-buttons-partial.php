1a67a2d40ded43564b52223ffa8b179a8c11073f
Satyadarshin Perry <stdn.engineer@gmail.com>
2024-11-14 12:42:43 +0000

    Attempted to store custom fields on user profile, and pull that into author page via ACF plugin. Proved over-complex; abandoned. Update to Social media buttons, and REAdMe.

--- partials/social-media-buttons-partial.php
+++ partials/social-media-buttons-partial.php
@@ -1,3 +1,6 @@
+<?php 
+$curauth = (isset($_GET['author_name'])) ? get_user_by('slug', $author_name) : get_userdata(intval($author));
+?>
 <div class="social-platform-set">
     <div class="panel social-platform">
         <div> 
@@ -5,22 +8,28 @@
         </div>
         <p>Follow me for more on my work history.</p>
     </div>
+    <?php if ( $curauth->facebook ): ?>
     <div class="panel social-platform">
        <div>
-           <a href="https://www.facebook.com/satyadarshin.is.interested.in/"><img src="<?php echo get_stylesheet_directory_uri(); ?>/images/facebook-logo-aluminium.png" title="" alt="Facebook icon" /></a>
+           <a href="<?php echo $curauth->facebook; ?>"><img src="<?php echo get_stylesheet_directory_uri(); ?>/images/facebook-logo-aluminium.png" title="" alt="Facebook icon" /></a>
        </div> 
         <p>Follow me for a diverse range of cultural interests: links to books, films, music and gigs, video essays, and podcasts that inspired me, and might move you too.</p>
     </div>
+   <?php endif; ?>
+   <?php if ( $curauth->instagram ): ?>
     <div class="panel social-platform">
         <div>
-            <a href="https://www.instagram.com/satyadarshin/"><img src="<?php echo get_stylesheet_directory_uri(); ?>/images/instagram-logo-aluminum.png" title="Personal, mainly street art documentary project" alt="Instagram icon"></a>
+            <a href="<?php echo $curauth->instagram; ?>"><img src="<?php echo get_stylesheet_directory_uri(); ?>/images/instagram-logo-aluminum.png" title="Personal, mainly street art documentary project" alt="Instagram icon"></a>
         </div>
         <p>Follow me for my documentary project about streeat art, mainly in Bristol.</p>
     </div> 
-        <?php 
-        /* TODO: open back up when Github is in a presentable state.
-        <a href="https://github.com/Satyadarshin"><img src="<?php echo get_stylesheet_directory_uri(); ?>/images/GitHub-Mark-64px.png" title="Visit GitHub for portfolio code projects" alt="GitHub icon"> 
-        */
-        ?>
-    </p>
+    <?php endif; ?>
+    <?php if ( $curauth->user_url ): ?>
+    <div class="panel social-platform">
+        <div>
+            <a href="<?php echo $curauth->user_url; ?>"><img src="<?php echo get_stylesheet_directory_uri(); ?>/images/github-logo-aluminum.png" title="Visit GitHub for portfolio projects" alt="GitHub icon" /></a> 
+        </div>
+        <p>My public Github repos.</p>
+    </div> 
+    <?php endif; ?>
 </div>
